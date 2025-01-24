1f45ae30bc9bf56dc134be9ee35859c79145ceca
Harshit Singhania <92168639+harshit-singhania@users.noreply.github.com>
2025-01-24 12:26:09 +0000

    apollo website initial commit

---
+++ webdev/apollo/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths as needed
+    ],
+    theme: {
+      extend: {
+        colors: {
+          apolloBlueMain: '#007c9d', // Add your custom hex color here
+        },
+      },
+    },
+    plugins: [],
+  };
